d81010f050b0ce07383925bc7617ae1d0f8f0d4a
Brett Zamir <brettz9@yahoo.com>
2020-05-18 23:13:22 +0800

    - Linting: As per latest ash-nazg/sauron-node - npm: Update devDeps - npm: Add `prepublishOnly` and `nyc` scripts - npm: Add linting to `test` script

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,19 @@ module.exports = {
       'Object.entries'
     ]
   },
-  extends: ['ash-nazg', 'plugin:node/recommended-script'],
+  extends: ['ash-nazg/sauron-node', 'plugin:node/recommended-script'],
   parserOptions: {
     sourceType: 'script'
   },
   rules: {
+    // Add back after overrides from Node plugin
+    'no-process-exit': 0,
+    'import/no-commonjs': 0,
+
+    // Browser only
+    'compat/compat': 0,
+
+    'node/exports-style': 0,
     'no-template-curly-in-string': 0
   }
 };
